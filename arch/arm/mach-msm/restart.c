c66fcee66e439834173607f30726262f8b4caeec
k5t4j5 <boogs19.kj@gmail.com>
2014-11-24 19:29:01 -0500

    Revert "msm: HTC: Add 8226 specific bits"
    This reverts commit e3116292f56196d311d4f968e952733595b7a179.

--- arch/arm/mach-msm/restart.c
+++ arch/arm/mach-msm/restart.c
@@ -479,9 +479,7 @@ void msm_restart(char mode, const char *cmd)
 		
 		msm_disable_wdog_debug();
 		halt_spmi_pmic_arbiter();
-#if defined(CONFIG_ARCH_MSM8226) && defined(CONFIG_HTC_DEBUG_WATCHDOG)
-		msm_watchdog_reset();
-#else
+#ifndef CONFIG_ARCH_MSM8226
 		__raw_writel(0, MSM_MPM2_PSHOLD_BASE);
 #endif
 	}
